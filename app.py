357f16c1ea4dd0c2433f55407b10f7506c36e27f
Cyrust Mods <45724743+cyrust21@users.noreply.github.com>
2025-04-22 15:39:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,12 @@ app = Flask(__name__)
 #DEFAULT_PASS = "3EC146CD4EEF7A640F2967B06D7F4413BD4FB37382E0ED260E214E8BACD96734"
 import os
 
+print("ENV UID:", os.getenv("DEFAULT_UID"))
+print("ENV PASS:", os.getenv("DEFAULT_PASS"))
+
 DEFAULT_UID = os.getenv("DEFAULT_UID")
 DEFAULT_PASS = os.getenv("DEFAULT_PASS")
+
 
 
 JWT_GEN_URL = "https://ariflexlabs-jwt-gen.onrender.com/fetch-token"
