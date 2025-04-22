3e2f4ceaa9ba5d983e1ca9726476a0872f3a05fe
Cyrust Mods <45724743+cyrust21@users.noreply.github.com>
2025-04-22 16:59:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -212,6 +212,7 @@ def get_player_info():
         }), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
    
   
